47bae68409906a5154521f4e3d6eae1ea1663ef7
Ryan Allen <ryan@yeahnah.org>
2007-11-17 08:51:47 +0000

    a loc rake task :)
    git-svn-id: svn://yeahnah.org/workflow/trunk@11 bd2358d6-94b4-11dc-a27b-d93154198e44

--- rakefile.rb
+++ rakefile.rb
@@ -5,3 +5,11 @@ end
 task :run_specs do
   puts `spec --require specs/bootstrap.rb --color --format specdoc specs/*_spec.rb`
 end
+
+task :loc do
+  loc, nf, blank = 0, 0, /^\s+?$/
+  Dir['**/*.rb'].each do |path|
+    open(path) { |f| nf += 1; f.each { |line| loc += 1 unless line =~ blank } }
+  end
+  puts "There are #{loc} non-blank line(s) of code across #{nf} file(s)."
+end
