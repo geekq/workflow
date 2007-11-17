a374b267f3813b9d660c37fae08a5cd2b210f9aa
Ryan Allen <ryan@yeahnah.org>
2007-11-17 04:55:51 +0000

    moving the specs around a bit
    git-svn-id: svn://yeahnah.org/workflow/trunk@6 bd2358d6-94b4-11dc-a27b-d93154198e44

--- specs/01_simple_state_spec.rb
+++ specs/01_simple_state_spec.rb
@@ -1,22 +1,16 @@
-describe 'a simple machine' do
+describe 'specifying states of a @machine' do
   
   setup do
     StateMachine.specify do
       state :new
       state :used
       state :broken
-      
-      initial_state :new
     end
     @machine = StateMachine.new
   end
   
-  it 'should have 3 states, [:new, :used, :broken]' do
+  it 'should have the specified states' do
     @machine.states.should == [:new, :used, :broken]
   end
-  
-  it 'should have :new as the current_state' do
-    @machine.current_state.should == :new
-  end
-  
+    
 end
