5e46b35de0afe233cda498a0371c402c4294fecd
Ryan Allen <ryan@yeahnah.org>
2007-11-17 04:49:28 +0000

    can specify initial state, current_state is set as initial state
    git-svn-id: svn://yeahnah.org/workflow/trunk@5 bd2358d6-94b4-11dc-a27b-d93154198e44

--- lib/state_machine.rb
+++ lib/state_machine.rb
@@ -18,29 +18,53 @@ module StateMachine
     
   end
   
+private
+  
   # implements the metaprogramming API for creating Specifications
   class Specifier
+    
     def initialize(&description)
+      instance_eval(&description)
     end
+    
+    def state(name)
+      (@states ||= []) << name
+    end
+    
+    def initial_state(name)
+      @initial_state = name
+    end
+    
     def to_specification
-      Specification.new
+      spec = Specification.new
+      spec.states = @states
+      spec.initial_state = @initial_state
+      spec
     end
+    
   end
   
   # describes a Machine and how it should work, can validate itself
   class Specification
-    def initialize
-    end
+    attr_accessor :states, :initial_state
   end
   
   # an instance of an actual machine, implementing the rest?
   class Machine
+    
     def initialize(specification)
       @specification = specification
+      @current_state = specification.initial_state
     end
+    
     def states
-      [:new, :used, :broken]
+      @specification.states
     end
+    
+    def current_state
+      @current_state
+    end
+    
   end
   
   # do we need classes for Actions/Events/Transitions/Etc? Perhaps
--- specs/01_simple_state_spec.rb
+++ specs/01_simple_state_spec.rb
@@ -5,12 +5,18 @@ describe 'a simple machine' do
       state :new
       state :used
       state :broken
+      
+      initial_state :new
     end
     @machine = StateMachine.new
   end
   
   it 'should have 3 states, [:new, :used, :broken]' do
     @machine.states.should == [:new, :used, :broken]
+  end
+  
+  it 'should have :new as the current_state' do
+    @machine.current_state.should == :new
   end
   
 end
