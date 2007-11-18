43c870cf68d2be89b28aa7a1d3656ce2dfbe9e8b
Ryan Allen <ryan@yeahnah.org>
2007-11-18 04:54:32 +0000

    adding stubbed specs, so we can work from home :)
    git-svn-id: svn://yeahnah.org/workflow/trunk@13 bd2358d6-94b4-11dc-a27b-d93154198e44

---
+++ specs/07_entry_and_exit_actions_on_transitions_to_state_spec.rb
@@ -0,0 +1,10 @@
+describe 'firing on_entry and on_exit actions for states when transitioned' do
+  
+  setup do
+    StateMachine.specify do
+      
+    end
+    @machine = StateMachine.new
+  end
+  
+end
