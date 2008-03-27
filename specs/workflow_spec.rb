31d6f1a27f2505be852faa4c1a794b7a1743ee1e
Ryan Allen <ryan@yeahnah.org>
2008-03-27 04:52:47 +0000

    more renaming of machine to workflow
    git-svn-id: svn://yeahnah.org/workflow/trunk@40 bd2358d6-94b4-11dc-a27b-d93154198e44

--- specs/workflow_spec.rb
+++ specs/workflow_spec.rb
@@ -1,4 +1,4 @@
-describe 'a very simple machine - two states, one event' do
+describe 'a very simple workflow - two states, one event' do
   
   setup do
     Workflow.specify do
@@ -7,25 +7,25 @@ describe 'a very simple machine - two states, one event' do
       end
       state :used
     end
-    @machine = Workflow.new
+    @workflow = Workflow.new
   end
 
   it 'should have two states' do
-    @machine.states.length.should == 2
+    @workflow.states.length.should == 2
   end
 
   it 'should have the first state as the initial state' do
-    @machine.current_state.should == @machine.find_state_by_name(:new)
+    @workflow.current_state.should == @workflow.find_state_by_name(:new)
   end
   
   it 'should transition to used when purchase even called' do
-    @machine.purchase
-    @machine.current_state.should == @machine.find_state_by_name(:used)
+    @workflow.purchase
+    @workflow.current_state.should == @workflow.find_state_by_name(:used)
   end
     
 end
 
-describe 'a machine with event actions' do
+describe 'a workflow with event actions' do
   
   setup do
     Workflow.specify do
@@ -44,30 +44,30 @@ describe 'a machine with event actions' do
         end
       end
     end
-    @machine = Workflow.new
-    @machine.extend(Recorder)
+    @workflow = Workflow.new
+    @workflow.extend(Recorder)
   end
   
-  it 'should run event action in context of machine' do
-    @machine.sell
-    @machine.records.last.should == 'Workflow::Machine was sold'
+  it 'should run event action in context of workflow' do
+    @workflow.sell
+    @workflow.records.last.should == 'Workflow::Machine was sold'
   end
   
-  it 'should pass in paramaters in context of machine' do
-    @machine.sell
-    @machine.auction(10)
-    @machine.records.last.should == 'Workflow::Machine w/ reserve of 10'
+  it 'should pass in paramaters in context of workflow' do
+    @workflow.sell
+    @workflow.auction(10)
+    @workflow.records.last.should == 'Workflow::Machine w/ reserve of 10'
   end
   
   it 'should not transition if action calls halt!' do
-    @machine.steal('nasty man')
-    @machine.records.last.should == "Workflow::Machine protecting against nasty man, the theif!"
-    @machine.current_state.should == @machine.find_state_by_name(:for_sale)
+    @workflow.steal('nasty man')
+    @workflow.records.last.should == "Workflow::Machine protecting against nasty man, the theif!"
+    @workflow.current_state.should == @workflow.find_state_by_name(:for_sale)
   end
   
 end
 
-describe 'a machine with on exit and on entry actions' do
+describe 'a workflow with on exit and on entry actions' do
   
   setup do
     Workflow.specify do
@@ -86,58 +86,58 @@ describe 'a machine with on exit and on entry actions' do
         end
       end
     end
-    @machine = Workflow.new
-    @machine.extend(Recorder)
+    @workflow = Workflow.new
+    @workflow.extend(Recorder)
   end
   
   it 'should trigger on_entry for taking_photo' do
-    @machine.speeding_car_detected
-    @machine.records.last.should == [@machine.find_state_by_name(:looking_for_speeders), :speeding_car_detected]
+    @workflow.speeding_car_detected
+    @workflow.records.last.should == [@workflow.find_state_by_name(:looking_for_speeders), :speeding_car_detected]
   end
   
   it 'should trigger on_exit for taking_photo' do
-    @machine.speeding_car_detected
-    @machine.photo_taken(:a_photo)
-    @machine.records.last.should == [@machine.find_state_by_name(:looking_for_speeders), :photo_taken, :a_photo]
+    @workflow.speeding_car_detected
+    @workflow.photo_taken(:a_photo)
+    @workflow.records.last.should == [@workflow.find_state_by_name(:looking_for_speeders), :photo_taken, :a_photo]
   end
   
   it 'should not execute on_entry or on_exit on halt!'
 end
 
-describe 'specifying and instanciating named state machines' do
+describe 'specifying and instanciating named state workflows' do
   
   setup do
-    Workflow.specify :alphabet_machine do
+    Workflow.specify :alphabet_workflow do
       state :a
       state :b
       state :c
     end
-    Workflow.specify :number_machine do
+    Workflow.specify :number_workflow do
       state :one
       state :two
       state :three
     end
-    @alphabet_machine = Workflow.new(:alphabet_machine)
-    @number_machine = Workflow.new(:number_machine)
+    @alphabet_workflow = Workflow.new(:alphabet_workflow)
+    @number_workflow = Workflow.new(:number_workflow)
   end
     
-  it 'should have states :a, :b, :c for @alphabet_machine' do
-    a = @alphabet_machine.find_state_by_name(:a)
-    b = @alphabet_machine.find_state_by_name(:b)
-    c = @alphabet_machine.find_state_by_name(:c)
-    @alphabet_machine.states.should == [a, b, c]
+  it 'should have states :a, :b, :c for @alphabet_workflow' do
+    a = @alphabet_workflow.find_state_by_name(:a)
+    b = @alphabet_workflow.find_state_by_name(:b)
+    c = @alphabet_workflow.find_state_by_name(:c)
+    @alphabet_workflow.states.should == [a, b, c]
   end
   
-  it 'should have states :one, :two, :three for @number_machine' do
-    one = @number_machine.find_state_by_name(:one)
-    two = @number_machine.find_state_by_name(:two)
-    three = @number_machine.find_state_by_name(:three)
-    @number_machine.states.should == [one, two, three]
+  it 'should have states :one, :two, :three for @number_workflow' do
+    one = @number_workflow.find_state_by_name(:one)
+    two = @number_workflow.find_state_by_name(:two)
+    three = @number_workflow.find_state_by_name(:three)
+    @number_workflow.states.should == [one, two, three]
   end
   
 end
 
-describe 'reconstitution of a machine (say, from a serialised object)' do
+describe 'reconstitution of a workflow (say, from a serialised object)' do
   
   setup do
     Workflow.specify do
@@ -145,19 +145,19 @@ describe 'reconstitution of a machine (say, from a serialised object)' do
       state :second
       state :third
     end
-    @machine = Workflow.reconstitute(:second)
+    @workflow = Workflow.reconstitute(:second)
   end
   
   it 'should reconstitute at second' do
-    @machine.current_state.should == @machine.find_state_by_name(:second)
+    @workflow.current_state.should == @workflow.find_state_by_name(:second)
   end
   
-  it 'should not execute on_entry when reconsituting a machine'
-  it 'should be possible to specify a named machine to reconsitute'
+  it 'should not execute on_entry when reconsituting a workflow'
+  it 'should be possible to specify a named workflow to reconsitute'
   
 end
 
-describe 'a machine with an on transition hook' do  
+describe 'a workflow with an on transition hook' do  
   
   setup do
     Workflow.specify do
@@ -168,27 +168,27 @@ describe 'a machine with an on transition hook' do
         record [from, to, triggering_event]+event_args
       end
     end
-    @machine = Workflow.new
-    @machine.extend(Recorder)
+    @workflow = Workflow.new
+    @workflow.extend(Recorder)
   end
   
   it 'should execute the hook on any transition of state, passing args' do
-    @machine.next(1) # => to :second
-    @machine.next(2) # => to :third
-    @machine.back(3) # => back to :second
-    first = @machine.find_state_by_name(:first)
-    second = @machine.find_state_by_name(:second)
-    third = @machine.find_state_by_name(:third)
-    @machine.records[0].should == [first, second, :next, 1]
-    @machine.records[1].should == [second, third, :next, 2]
-    @machine.records[2].should == [third, second, :back, 3]
+    @workflow.next(1) # => to :second
+    @workflow.next(2) # => to :third
+    @workflow.back(3) # => back to :second
+    first = @workflow.find_state_by_name(:first)
+    second = @workflow.find_state_by_name(:second)
+    third = @workflow.find_state_by_name(:third)
+    @workflow.records[0].should == [first, second, :next, 1]
+    @workflow.records[1].should == [second, third, :next, 2]
+    @workflow.records[2].should == [third, second, :back, 3]
   end
   
   it 'should not execute hook on halt!'
   it 'should act like a chain so we can go on_transition on_transition...'
 end
 
-describe 'binding machines to another context' do
+describe 'binding workflowss to another context' do
   
   setup do
     Workflow.specify do
@@ -216,41 +216,41 @@ describe 'binding machines to another context' do
         "you hit #{method.inspect}"
       end
     end
-    @machine = Workflow.new
-    @machine.bind_to(@context)
+    @workflow = Workflow.new
+    @workflow.bind_to(@context)
   end
   
   it 'should execute event actions in context' do
-    @machine.next(:a)
+    @workflow.next(:a)
     @context.records.should include(:a)
   end
   
   it 'should execute on_entry in context' do
-    @machine.next(:a)
-    @machine.next(:b)
+    @workflow.next(:a)
+    @workflow.next(:b)
     @context.records.should include('entered :third')
   end
   
   it 'should execute on_exit in context' do
-    @machine.next(:a)
-    @machine.next(:b)
-    @machine.next(:c)
+    @workflow.next(:a)
+    @workflow.next(:b)
+    @workflow.next(:c)
     @context.records.should include('exited :third')
   end
   
   it 'should execute on_transition in context' do
-    @machine.next(:a)
+    @workflow.next(:a)
     @context.records.should include('transitioned from first to second')
   end
   
   it 'should have a current_state accessor' do
-    @context.current_state.should == @machine.find_state_by_name(:first)
+    @context.current_state.should == @workflow.find_state_by_name(:first)
   end
   
   it 'should chain-patch method_missing to respond to events' do
     @context.next(:a)
     @context.x.should == 'you hit :x'
-    @context.current_state.should == @machine.find_state_by_name(:second)
+    @context.current_state.should == @workflow.find_state_by_name(:second)
   end
   
   it 'should support blocks with method missing too!'
@@ -264,8 +264,8 @@ end
 #
 
 describe 'the class integration mixin' do
-  it 'should set up a state_machine class method for describing the machine'
-  it 'should instanciate a machine on initialize'
+  it 'should set up a workflow class method for describing the workflow'
+  it 'should instanciate a workflow on initialize'
   it 'should behave like a typical binding'
   it 'should work with inhertiance'
 end
@@ -275,7 +275,7 @@ describe 'active record integration mixin' do
   it 'should handle serializing '
 end
 
-describe 'reflecting machines' do
+describe 'reflecting workflows' do
   it 'should allow you to intuitively reflect states'
   it 'should allow you to intuitively reflect allowable events in states'
   it 'should allow attachment of meta data to states and state events'
