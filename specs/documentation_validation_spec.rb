1fb4b373457141f27bde2fcc824f798326bca0bd
Ryan Allen <ryan@yeahnah.org>
2008-03-28 04:33:18 +0000

    bunch load more specs :)
    git-svn-id: svn://yeahnah.org/workflow/trunk@42 bd2358d6-94b4-11dc-a27b-d93154198e44

--- specs/documentation_validation_spec.rb
+++ specs/documentation_validation_spec.rb
@@ -1,6 +1,3 @@
-# %w(rubygems spec).each { |lib| require lib }
-# require '../workflow'
-
 describe 'what is in the README' do
   
   setup do
@@ -9,13 +6,23 @@ describe 'what is in the README' do
         event :submit, :transitions_to => :awaiting_review
       end
       state :awaiting_review do
-        event :review, :transitions_to => :being_reviewed
+        event :review, :transitions_to => :being_reviewed do |reviewer|
+          reviewer << 'oi!'
+        end
+        
       end
       state :being_reviewed do
         event :accept, :transitions_to => :accepted
         event :reject, :transitions_to => :rejected
+        on_exit do |new_state, event_fired, *event_args|
+          $new_state = new_state
+          $event_fired = event_fired 
+          $event_args = event_args
+        end
       end
-      state :accepted
+      state :accepted do
+        
+      end
       state :rejected
     end
     @workflow = Workflow.new('Article Workflow')
@@ -36,6 +43,42 @@ describe 'what is in the README' do
     @workflow.submit
     @workflow.new?.should == false
     @workflow.awaiting_review?.should == true
+  end
+  
+  it 'should do stuff with args to events' do
+    @workflow.submit
+    @reviewer = ''
+    @workflow.review(@reviewer)
+    @reviewer.should == 'oi!'
+  end
+  
+  it 'should be like, cool with on_exit'
+  it 'should be like, cool with on_entry'
+  it 'should be like, cool with on_transition'
+  it 'should halt'
+  it 'should halt!'
+  it 'halts with messages'
+  it 'halts! with messages'
+  it 'reflects states'
+  it 'reflects events of a state'
+  it 'reflects transitions_to of a state'
+  it 'has meta relfection on states'
+  it 'has meta relfection on events'
+  it 'can iterate over state meta'
+  it 'can iterate over event meta'
+  it 'orders firing like action -> on_transition -> on_exit -> TRANSITION -> on_entry'
+
+  describe 'class integration' do
+    it 'has a meta method called workflow'
+    it 'has a workflow method on the instances'
+    it 'proxies states to the workflow'
+    it 'proxies events to the workflow'
+    it 'has the instance as the action scope'
+  end
+  
+  describe 'AR integration' do
+    it 'serializes state on_transition ?'
+    it 'reconsitutes from state on find ?'
   end
   
 end
