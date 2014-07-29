fd1ecbc213612185a7e73f2ec64340a584351c49
Rob Howard <rob@robhoward.id.au>
2014-07-29 12:36:05 +1000

    Adds workflow_adapter hook support.

--- lib/workflow.rb
+++ lib/workflow.rb
@@ -262,11 +262,16 @@ module Workflow
 
     klass.extend ClassMethods
 
-    if Object.const_defined?(:ActiveRecord) && klass < ActiveRecord::Base
-      klass.send :include, Adapter::ActiveRecord
-    end
-    if Object.const_defined?(:Remodel) && klass < Adapter::Remodel::Entity
-      klass.send :include, Adapter::Remodel::InstanceMethods
+    # Look for a hook; otherwise detect based on ancestor class.
+    if klass.respond_to?(:workflow_adapter)
+      klass.send :include, klass.workflow_adapter
+    else
+      if Object.const_defined?(:ActiveRecord) && klass < ActiveRecord::Base
+        klass.send :include, Adapter::ActiveRecord
+      end
+      if Object.const_defined?(:Remodel) && klass < Adapter::Remodel::Entity
+        klass.send :include, Adapter::Remodel::InstanceMethods
+      end
     end
   end
 end
