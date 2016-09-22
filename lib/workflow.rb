b895edd7d8b3fefa6b0bef0aca5071978e4b8856
Tyler Gannon <tyler@aprilseven.co>
2016-09-22 19:09:11 +0800

    Always call persist_workflow_state!

--- lib/workflow.rb
+++ lib/workflow.rb
@@ -93,9 +93,9 @@ module Workflow
         named_arguments: workflow_spec.named_arguments
 
       run_all_callbacks do
-        callback_value = run_action_callback name, *args
-        return_value   = callback_value
-        return_value ||= persist_workflow_state(target.name) || true
+        callback_value  = run_action_callback name, *args
+        persist_value   = persist_workflow_state(target.name)
+        callback_value || persist_value
       end
     ensure
       @transition_context = nil
