7d37ce6c4ac6b295916fe98477e601e50e0c51da
Vladimir Dobriakov <vladimir@geekq.net>
2025-10-16 09:39:23 +0200

    Revert "Update conditional argument handling to correctly support lambdas"
    This reverts commit 24212f4ad2036d0378b14c830f454261fdf7c977.

--- test/conditionals_test.rb
+++ test/conditionals_test.rb
@@ -92,8 +92,7 @@ class ConditionalsTest < Minitest::Test
           event :turn_on, :transitions_to => :low_battery # otherwise
         end
         state :on do
-          # Use a lambda proc, which enforces correct arity
-          event :check, :transitions_to => :low_battery, :if => -> (obj) { return false }
+          event :check, :transitions_to => :low_battery, :if => proc { |obj| return false }
           event :check, :transitions_to => :on # stay in on state otherwise
         end
         state :low_battery
